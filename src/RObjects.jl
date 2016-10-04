3cb46efcbaa1e834f909fd6e0a4f1a8d073dfd49
mkborregaard <mkborregaard@snm.ku.dk>
2016-10-04 21:50:13 +0200

    Introduced issue with kwargs

--- src/RObjects.jl
+++ src/RObjects.jl
@@ -8,14 +8,14 @@ function getRobject(name::String)
     #        obj$shape <- as.matrix(obj$shape)
     #    } # the idea is to later support shape types - not now
     obj$sitestat = obj$coords@data
-    obj$coords = sp::coordinates(dat$coords)
+    obj$coords = sp::coordinates(obj$coords)
     """
     rcopy(R"obj")
 end
 
 function Assemblage{T <: Symbol, S}(rdict::Dict{T, S})
     cd_type = rdict[:type] == "grid" ? griddata : pointdata #this code only works as long as there are only those two types
-    Assemblage(rdict[:comm], rdict[:coords] )#,
-        #cdtype = cd_type, traits = rdict[:species_stats],
-        #sitestats = rdict[:sitestat])
+    Assemblage(rdict[:comm], rdict[:coords] ,
+        cdtype = cd_type, traits = rdict[:species_stats],
+        sitestats = rdict[:sitestat])
 end
