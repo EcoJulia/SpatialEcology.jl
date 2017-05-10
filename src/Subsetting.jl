ced9ade51302e505975b2c4b7ca4c2611b4f2e47
Michael K. Borregaard <mkborregaard@snm.ku.dk>
2017-05-10 15:54:16 +0200

    Add show methods for view types

--- src/Subsetting.jl
+++ src/Subsetting.jl
@@ -95,64 +95,14 @@ function subsetgrid(indices, grid)
   GridTopology(xmin, grid.xcellsize, xcells, ymin, grid.ycellsize, ycells)
 end
 
+function show(io::IO, com::SubComMatrix)
+    sp = createsummaryline(specnames(com))
+    si = createsummaryline(sitenames(com))
+    println("SubComMatrix with $(records(com)) records of $(nspecies(com)) species in $(nsites(com)) sites\n\nSpecies names:\n$(sp)\n\nSite names:\n$(si)")
+end
 
-
-
-######## OLD #####################
-# subset!(occ::OccFields, species = 1:nspecies(occ), sites = 1:nsites(occ)) = OccFields(occ.commatrix[sites, species], occ.traits[species,:])
-# function subset(occ::OccFields, species = 1:nspecies(occ), sites = 1:nsites(occ))
-#     ret = deepcopy(occ)
-#     subset!(ret, species, sites)
-#     ret
-# end
-#
-# subset!(site::SiteFields, sites = 1:nsites(occ)) = dropbyindex!(site, sites) #TODO maybe replace dropbyindex with subset! to begin with
-# function subset(site::SiteFields, sites = 1:nsites(occ))
-#     ret = deepcopy(site)
-#     subset!(ret, sites)
-#     ret
-# end
-#
-# subset(asm::Assemblage; species = 1:nspecies(asm), sites = 1:nsites(asm), dropemptyspecies = true, dropemptysites = true) = Assemblage(subset(asm.site, sites), subset(asm.occ, species, sites), dropemptysites = dropemptysites, dropemptyspecies = dropemptyspecies)
-# subset!(asm::Assemblage; species = 1:nspecies(asm), sites = 1:nsites(asm), dropemptyspecies = true, dropemptysites = true) = Assemblage(subset!(asm.site, sites), subset!(asm.occ, species, sites), dropemptysites = dropemptysites, dropemptyspecies = dropemptyspecies)
-#
-# subset!(sp::SiteData, sites = 1:nsites(sp)) = SiteData(subset!(sp.site, sites))
-# subset(sp::SiteData, sites = 1:nsites(sp)) = SiteData(subset(sp.site, sites))
-#
-
-
-
-
-
-# @enum keepsite allsites occupied
-# @enum keepspecies allspecies present
-#
-# function subset!(occ::OccFields, species = present, sites = occupied)
-#     keepsites = false
-#     sites == allsites && keepsites = true
-#     (keepsites || sites == occupied) && sites = 1:nsites(occ)
-#
-#     keepspecies = false
-#     species == allspecies && keepspecies = true
-#     (keepspecies || species == present) && species = 1:nspecies(occ)
-#
-#     occ.commatrix = occ.commatrix[sites, species]  #There are three big allocations of the same array here - this code should be rewritten to use views instead so most can be done inplace
-#     keepspecies || occ.commatrix = occ.commatrix[:, occupancy(occ.commatrix) .> 0]
-#     keepsites || occ.commatrix = occ.commatrix[richness(occ.commatrix) .> 0, :]
-#
-#     #it'd be smarter to do this with indices, I think, as I also need to subset the other bits.
-# end
-#
-#
-#
-# function subset!(site::SiteFields, sites)
-#     site.coords = site.coords[sites, :]
-#     site.sitestats = site.sitestats[sites, :]
-# end
-#
-# subset(site::SiteFields, sites) = subset!(deepcopy(site), sites)
-#
-# function subset!(asm::Assemblage; species = present, sites = occupied)
-#     subset!(asm.occ, species, sites)
-#     subset!(asm.site, findin(sitenames(asm.site, sitenames(asm.occ))))
-# end
+function show(io::IO, com::SubAssemblage)
+    sp = createsummaryline(specnames(com))
+    si = createsummaryline(sitenames(com))
+    println("SubAssemblage with $(records(com)) records of $(nspecies(com)) species in $(nsites(com)) sites\n\nSpecies names:\n$(sp)\n\nSite names:\n$(si)")
+end
