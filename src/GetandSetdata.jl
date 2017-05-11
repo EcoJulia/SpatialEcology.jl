678764687c39e796270b1cc24afc4f8ee0ee6d22
Michael K. Borregaard <mkborregaard@snm.ku.dk>
2017-05-11 08:08:15 +0200

    Fixed reporting bug in addtraits

--- src/GetandSetdata.jl
+++ src/GetandSetdata.jl
@@ -17,7 +17,7 @@ sitestatnames(asm::Assmbl) = names(sitestats(asm))
 
 function addtraits!(asm::Assemblage, newtraits::DataFrames.DataFrame, species::Symbol; validate = true, tolerance = 0.5)
     if validate
-        dif, left, right = length(intersect(newtraits[species], specnames(asm))) , size(newtraits,1), nspecies(asm)
+        dif, left, right = length(intersect(newtraits[species], specnames(asm))), nspecies(asm), size(newtraits,1)
         max(dif/left, dif/right) == 0 && error("No match between species names, aborting join")
         println("$dif matching species names,\n",
                 "\t$(signif(100*dif/left,3))% of $left species in the Assemblage\n",
@@ -34,7 +34,7 @@ end
 
 function addsitestats!(asm::Assemblage, newsites::DataFrames.DataFrame, sites::Symbol; validate = true, tolerance = 0.5)
     if validate
-        dif, left, right = length(intersect(newsites[sites], sitenames(asm))) , size(newsites,1), nsites(asm)
+        dif, left, right = length(intersect(newsites[sites], sitenames(asm))) , nsites(asm), size(newsites,1)
         max(dif/left, dif/right) == 0 && error("No match between site names, aborting join")
         println("$dif matching site names,\n",
                 "\t$(signif(100*dif/left,3))% of $left sites in the Assemblage\n",
