7fbffc937ec37bb6fe05c6544e72f396904a3902
Michael K. Borregaard <mkborregaard@snm.ku.dk>
2017-05-16 13:25:30 +0200

    export new getter functions

--- src/SpatialEcology.jl
+++ src/SpatialEcology.jl
@@ -14,7 +14,9 @@ import MacroTools: @capture
 
 export SiteData, ComMatrix, Assemblage, coordtype, DispersionField #types and their constructors
 export nspecies, nsites, occupancy, richness, records, sitenames, specnames, coordinates
-export traits, sitestats, sitestatnames, traitnames
+export occurring, noccurring, occupied, noccupied
+export traits, sitestats, sitestatnames, traitnames, commatrix
+export addtraits!, addsitestats!
 export copy, setindex!, getindex, size, show, summary, view
 export coordstype, subset
 export xcells, ycells, cells, xmin, xmax, ymin, ymax, xrange, yrange, xcellsize, ycellsize, cellsize, boundingbox #it is possible that I will export none of these
@@ -23,6 +25,7 @@ export getRobject
 include("DataTypes.jl")
 include("Constructor_helperfunctions.jl")
 include("Constructors.jl")
+include("Sparse_matrixfunctions.jl")
 include("Commatrix_functions.jl")
 include("GetandSetdata.jl")
 include("Gridfunctions.jl")
