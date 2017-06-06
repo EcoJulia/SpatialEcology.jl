3f4af5cb79504adbbfe4146a2c6fdf0a851e8740
Michael K. Borregaard <mkborregaard@snm.ku.dk>
2017-06-06 17:10:30 +0200

    fix tests typo

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,6 +7,6 @@ amph = Assemblage(amphdat[4:end], amphdat[1:3])
 @test extrema(richness(amph)) == (1, 20)
 @test nsites(amph) == 1010
 @test noccupied(amph) == 1010
-@test nsites(amph) == 73
+@test nspecies(amph) == 73
 @test noccurring(amph) == 73
 @test occurring(amph, 718) == [15]
