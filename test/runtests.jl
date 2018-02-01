5f40d7bdacf7bfeb7b3543f6a2c1323a3b76e279
Michael K. Borregaard <mkborregaard@snm.ku.dk>
2018-02-01 09:48:48 +0100

    Use CSV to load test data

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,10 @@
 using SpatialEcology
 using DataFrames
+using CSV
 using Base.Test
 
 # write your own tests here
-amphdat = readtable("../data/amph_Europe.csv")
+amphdat = CSV.read("../data/amph_Europe.csv")
 amph = Assemblage(amphdat[4:end], amphdat[1:3], sitecolumns = false)
 @test extrema(richness(amph)) == (1, 20)
 @test nsites(amph) == 1010
