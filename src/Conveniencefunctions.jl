c873bda99e3ee7e90e84252659f7407dddba3977
Michael K. Borregaard <mkborregaard@snm.ku.dk>
2018-03-22 12:23:01 +0000

    Bug fix in asquantiles

--- src/Conveniencefunctions.jl
+++ src/Conveniencefunctions.jl
@@ -1,13 +1,13 @@
 
 function asquantiles!(x::AbstractVector, n::Int)
-    quants = nquantile(x, n)
+    quants = nquantile(x, n)[1:end-1]
     for (i,j) in enumerate(x)
         x[i] = searchsortedlast(quants, j)
     end
 end
 
 function asquantiles(x::AbstractVector, n::Int)
-    quants = nquantile(x, n)
+    quants = nquantile(x, n)[1:end-1]
     ret = similar(x)
     for (i,j) in enumerate(x)
         ret[i] = searchsortedlast(quants, j)
