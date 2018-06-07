b393e7f8a07be80ac944ab73845d0395cc7cf3ae
Michael K. Borregaard <mkborregaard@snm.ku.dk>
2018-06-07 15:42:01 +0200

    Add some comments to runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,9 +3,13 @@ using DataFrames
 using CSV
 using Base.Test
 
-# write your own tests here
+
 amphdat = CSV.read("../data/amph_Europe.csv")
+
+# constructors
 amph = Assemblage(amphdat[4:end], amphdat[1:3], sitecolumns = false)
+
+# accesseors
 @test extrema(richness(amph)) == (1, 20)
 @test nsites(amph) == 1010
 @test noccupied(amph) == 1010
@@ -13,4 +17,5 @@ amph = Assemblage(amphdat[4:end], amphdat[1:3], sitecolumns = false)
 @test noccurring(amph) == 73
 @test occurring(amph, 718) == [15]
 
+# views
 va = view(amph, species = 1:10)
