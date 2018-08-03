e57ea5ce59f1fc494e5ff645d963cbf5d18e103c
Kevin Bonham <kevbonham@gmail.com>
2018-08-03 13:54:12 -0400

    Fix error on @forward_func

--- src/Commatrix_functions.jl
+++ src/Commatrix_functions.jl
@@ -1,6 +1,7 @@
 
 macro forward_func(ex, fs)
-    T, field = ex.args[1], ex.args[2].args[1]
+    T, field = ex.args[1], ex.args[2].value
+
     T = esc(T)
     fs = isexpr(fs, :tuple) ? map(esc, fs.args) : [esc(fs)]
     :($([:($f(x::$T, args...) = (Base.@_inline_meta; $f(x.$field, args...)))
