5923ac6dfca274f159b9e1d356214c13dacc3a2c
Michael Krabbe Borregaard <mkborregaard@snm.ku.dk>
2019-01-06 11:58:17 +0100

    depend on DataFramesMeta

--- src/SpatialEcology.jl
+++ src/SpatialEcology.jl
@@ -2,6 +2,7 @@ __precompile__(true)
 module SpatialEcology
 
 using DataFrames
+import DataFramesMeta
 using SparseArrays
 using Statistics
 
@@ -27,6 +28,7 @@ export coordstype, subset
 export xcells, ycells, cells, xmin, xmax, ymin, ymax, xrange, yrange, xcellsize, ycellsize, cellsize, boundingbox #it is possible that I will export none of these
 export sitetotals, speciestotals, getspecies, getsite
 export groupspecies, groupsites
+export DataFramesMeta.@with
 
 include("DataTypes.jl")
 include("Constructor_helperfunctions.jl")
