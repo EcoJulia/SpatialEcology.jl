b28b37e12b9bbc1e3d64e70cc2821af994f89e80
Michael Krabbe Borregaard <mkborregaard@snm.ku.dk>
2019-01-07 15:26:22 +0100

    add randomization for commatrices

--- src/Randomizations.jl
+++ src/Randomizations.jl
@@ -1,6 +1,7 @@
 import Random
 
-
+matrixrandomizer(com::ComMatrix, rng = Xoroshiro128Plus();
+                method::matrixrandomizations = curveball) = "Only defined for Boolean Assemblages"
 matrixrandomizer(asm::SEAssemblage, rng = Xoroshiro128Plus();
                 method::matrixrandomizations = curveball) = "Only defined for Boolean Assemblages"
 
@@ -12,8 +13,22 @@ function matrixrandomizer(asm::S, rng = Xoroshiro128Plus();
     ret
 end
 
-Random.rand(r::MatrixGenerator{R, Assemblage}) where R = copy(rand!(r))
+function matrixrandomizer(com::C, rng = Xoroshiro128Plus();
+                        method::matrixrandomizations = curveball) where C <: ComMatrix{Bool}
+    cm = copy(com)
+    ret = MatrixGenerator{typeof(rng), typeof(cm)}(as, method, rng)
+    dropzeros!(ret.m.occurrences)
+    ret
+end
+
+Random.rand(r::MatrixGenerator{R, <:Assemblage}) where R = copy(rand!(r))
 function Random.rand!(r::MatrixGenerator{R, A}) where {R} where {A <: Assemblage}
     RandomBooleanMatrices._curveball!(r.m.occ.commatrix.occurrences)
+    r.m
+end
+
+Random.rand(r::MatrixGenerator{<:ComMatrix}) where R = copy(rand!(r))
+function Random.rand!(r::MatrixGenerator{C}) where {A <: ComMatrix}
+    RandomBooleanMatrices._curveball!(r.m.occurrences)
     r.m
 end
