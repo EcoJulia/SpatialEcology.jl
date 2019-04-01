471dae0a838fcc93c35602679582c4ba5894bbda
Michael Krabbe Borregaard <mkborregaard@snm.ku.dk>
2019-04-01 22:56:59 +0200

    finish implementing aggregate

--- src/Operations.jl
+++ src/Operations.jl
@@ -9,14 +9,15 @@ aggregate(gr::GridTopology, factor) = GridTopology(gr.xs[1]:step(gr.xs)*factor:g
 #aggregate(asm::SEAssemblage{D, T, P}, factor::Integer, fun = :auto; data_function = :auto) where D where T where P <: SEGrid =
 #    Assemblage{D, GridData}(aggregate(asm.site, factor, fun; data_function), aggregate_grid(asm.occ, factor, fun; data_function))
 
-aggregate(gr::SEGrid, factor::Integer) = GridData(ceil.(Int, gr.indices ./ factor), aggregate(gr.grid, factor))
-function aggregate(gr::SEGrid, newgrid::GridTopology)
+aggregate(gr::SEGrid, factor::Integer) = (g = aggregate(gr.grid, factor); aggregate(gr, g))
+aggregate(gr::SEGrid, newgrid::GridTopology) = (ind = apply_grid(gr, newgrid); GridData(sortslices(unique(ind, dims = 1), dims = 1), newgrid))
+
+function apply_grid(gr::SEGrid, newgrid::GridTopology)
     factor = [cellsize(newgrid)...] ./ cellsize(gr)
     shift = [xmin(newgrid) - xmin(gr), ymin(newgrid) - ymin(gr)] .% factor ./cellsize(gr)
     newind = ceil.(Int, (shift' .+ gr.indices) ./ factor')
-    newind, newgrid
+    newind
 end
-
 
 replace_auto(x, ::Type{Bool}) = x == :auto ? Base.any : x
 replace_auto(x, ::Type{Integer}) = x == :auto ? Base.sum : x
@@ -24,35 +25,23 @@ replace_auto(x, ::Type{Integer}) = x == :auto ? Base.sum : x
 function aggregate(asm::SEAssemblage{D, T, P}, factor::Integer, fun = :auto) where D where T where P <: SELocations{<: SEGrid}
     _fun = replace_auto(fun, D)
     _fun == :auto && error("Default aggregation functions are only defined for Bool (presence-absence) and Int (abundance) commatrices")
-    newx, newy = ceil.(Int,cells(asm)./factor)
-    newsites = newx*newy
-    retmat = spzeros(D, nspecies(asm), newsites)
-    #_fun(x) = _fun(filter(y -> isfinite(y) && !ismissing(y), x)) not sure I have missing values
 
-    for j in 1:newx-1
-       for i in 1:newy-1
-          newi, newj = (i-1)*factor .+ (1:factor), (j-1)*factor .+ (1:factor)
-          window = findall(x->((ni,nj) = indices(asm, x); ni ∈ newi && nj ∈ newj), 1:nsites(asm))
-          length(window) > 0 &&
-            (retmat[:, (j-1)*newy + i] = mapslices(_fun, view(occurrences(asm), :, window), dims = 2))
-       end
-       newi, newj = (newx-1)*factor .+ (1:xcells(asm)), (j-1)*factor .+ (1:factor)
-       window = findall(x->((ni,nj) = indices(asm, x); ni ∈ newi && nj ∈ newj), 1:nsites(asm))
-       length(window) > 0 &&
-         (retmat[:, j*newy] = mapslices(_fun, view(occurrences(asm), :, window), dims = 2))
+    # create the new grid
+    gt = aggregate(asm.site.coords.grid, factor)
+    tmpgrid = apply_grid(asm.site.coords, gt)
+    tmpsites = sortslices(unique(tmpgrid, dims = 1), dims = 1)
+
+    # fill a new occurrence matrix
+    retmat = spzeros(D, nspecies(asm), size(tmpsites, 1))
+    for newcell in axes(tmpsites, 1)
+        inds = findall(x->tmpgrid[x, :] == tmpsites[newcell, :], 1:size(tmpgrid, 1))
+        retmat[:, newcell] = mapslices(_fun, view(occurrences(asm), :, inds), dims = 2)
     end
 
-    for i in 1:newy-1
-      newi, newj = (newx-1)*factor + 1:xcells(asm), (newy-1)*factor .+ (1:ycells(asm))
-      window = findall(x->((ni,nj) = indices(asm, x); ni ∈ newi && nj ∈ newj), 1:nsites(asm))
-      length(window) > 0 &&
-        (retmat[:, (newx-1)*newy + i] = mapslices(_fun, view(occurrences(asm), :, window), dims = 2))
-    end
-
-    newsitenames = string.(["agg_"], 1:newsites)
-    @show size(retmat)
+    #build the basic objects
+    newsitenames = string.(["agg_"], 1:size(retmat, 2))
+    sit = Locations{GridData}(GridData(tmpsites, gt), DataFrame(sites = newsitenames))
     retcommat = ComMatrix{D}(retmat, speciesnames(asm), newsitenames)
     sp = SpeciesData(retcommat, traits(asm))
-    sit = aggregate(asm.site, factor)
     Assemblage(sit,sp)
 end
