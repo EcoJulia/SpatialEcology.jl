3ca589ca56b7547059d95269cbaa1756a2c3e402
Michael Krabbe Borregaard <mkborregaard@snm.ku.dk>
2019-04-01 18:47:41 +0200

    better show functions for grids

--- src/Gridfunctions.jl
+++ src/Gridfunctions.jl
@@ -5,19 +5,22 @@ ycellsize(g::GridTopology) = step(g.ys)
 xcells(g::GridTopology) = length(g.xs)
 ycells(g::GridTopology) = length(g.ys)
 indices(g::SEGrid) = g.indices
-indices(g::SEGrid, idx) = g.indices[:,idx]
+indices(g::SEGrid, idx) = g.indices[idx,:]
 boundingbox(g::GridTopology) = Bbox(xmin(g), xmax(g), ymin(g), ymax(g))
 
-@forward_func GridData.grid xmin, ymin, xcellsize, ycellsize, cellsize, xcells, ycells, cells, xrange, yrange, xmax, ymax, boundingbox
-@forward_func Locations{GridData}.coords xmin, ymin, xcellsize, ycellsize, cellsize, xcells, ycells, cells, xrange, yrange, xmax, ymax, boundingbox
-@forward_func SubGridData.grid xmin, ymin, xcellsize, ycellsize, cellsize, xcells, ycells, cells, xrange, yrange, xmax, ymax, boundingbox
-@forward_func SubLocations{SubGridData}.coords xmin, ymin, xcellsize, ycellsize, cellsize, xcells, ycells, cells, xrange, yrange, xmax, ymax, boundingbox
+@forward_func GridData.grid xmin, ymin, xcellsize, ycellsize, xcells, ycells, boundingbox
+@forward_func Locations{GridData}.coords xmin, ymin, xcellsize, ycellsize, xcells, ycells, boundingbox, indices
+@forward_func SubGridData.grid xmin, ymin, xcellsize, ycellsize, xcells, ycells, boundingbox
+@forward_func SubLocations{SubGridData}.coords xmin, ymin, xcellsize, ycellsize, xcells, ycells, boundingbox, indices
+@forward_func SEAssemblage.site xmin, ymin, xcellsize, ycellsize, xcells, ycells, boundingbox, indices
 
 
-show(io::IO, b::Bbox) = println(io, "xmin:\t$(b.xmin)\nxmax:\t$(b.xmax)\nymin:\t$(b.ymin)\nymax:\t$(b.ymax)")
-show(io::IO, g::GridData) = println(io,
+show(io::IO, b::Bbox) = println(io, "xmin:\t$(b.xmin)\nxzmax:\t$(b.xmax)\nymin:\t$(b.ymin)\nymax:\t$(b.ymax)")
+show(io::IO, g::GridData) = (println(io, "Spatial Grid\n", gridline(g.grid)); println(io, "\n", size(g.indices,1), " sites");)
+show(io::IO, g::GridTopology) = println(io, "Spatial GridTopology\n", gridline(g))
+gridline(g::GridTopology) =
     """
-    Spatial grid
        lower left : $(xmin(g)), $(ymin(g))
        cellsizes  : $(xcellsize(g)), $(ycellsize(g))
-       size       : $(xcells(g)), $(ycells(g))""")
+       size       : $(xcells(g)), $(ycells(g))
+    """
