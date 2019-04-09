ad784eb3cae24d429ca73f406a82fc10fcaf852b
Michael Krabbe Borregaard <mkborregaard@snm.ku.dk>
2019-04-09 09:02:22 +0200

    Allow passing a Tuple to `factor`

--- src/Operations.jl
+++ src/Operations.jl
@@ -17,7 +17,7 @@ aggregate(gr::GridTopology, factor) = _grid_from_factor(gr, factor)
 aggregate(gr::SEGrid, factor::Integer) = (g = _grid_from_factor(gr.grid, factor); aggregate(gr, g))
 aggregate(gr::SEGrid, newgrid::GridTopology) = (ind = _apply_grid(gr, newgrid); GridData(sortslices(unique(ind, dims = 1), dims = 1), newgrid))
 
-function aggregate(asm::SEAssemblage{D, T, P}, factor::Integer, fun = _default_fun(D)) where D where T where P <: SELocations
+function aggregate(asm::SEAssemblage{D, T, P}, factor::Union{Integer, Tuple{Integer, Integer}}, fun = _default_fun(D)) where D where T where P <: SELocations
 
     gt = _grid_from_factor(asm.site.coords, factor)
     aggregate(asm, gt, fun)
@@ -65,7 +65,7 @@ function _grid_from_factor(pt::SEPoints, factor)
     GridTopology(_range_from_factor(xmi, xma, first(factor)), _range_from_factor(ymi, yma, last(factor)))
 end
 _grid_from_factor(gd::SEGrid, factor) = _grid_from_factor(gd.grid, factor)
-_grid_from_factor(gd::GridTopology, factor) = GridTopology(_range_from_factor(extrema(gd.xs)..., factor), _range_from_factor(extrema(gd.ys)..., factor))
+_grid_from_factor(gd::GridTopology, factor) = GridTopology(_range_from_factor(extrema(gd.xs)..., first(factor)), _range_from_factor(extrema(gd.ys)..., last(factor)))
 
 _default_fun(::Type{Bool}) = Base.any
 _default_fun(::Type{Integer}) = Base.sum
