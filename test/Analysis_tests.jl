ade9c22c1f74abd81088237b59437b655661825c
Kevin Bonham, PhD <kevbonham@gmail.com>
2020-08-12 16:13:44 -0400

    add 1.5 to tests

--- test/Analysis_tests.jl
+++ test/Analysis_tests.jl
@@ -8,7 +8,7 @@ using Plots
     amphdat = CSV.read(joinpath(dirname(pathof(SpatialEcology)), "..", "data", "amph_Europe.csv"), DataFrame)
     amph = Assemblage(amphdat[!,4:end], amphdat[!,1:3], sitecolumns = false)
     addtraits!(amph, asquantiles(occupancy(amph), 4), :quantile)
-    gb = groupspecies(amph, :quantile)
+    gb = groupspecies(amph, :quantile) # error ?
     ps = [plot(g) for g in gb]
     p = ps[4];
 
